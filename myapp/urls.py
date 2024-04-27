4ca7faabdbcbb5cffd36e51ab68ed409c43ff9ad
Harshith0112 <141983280+Harshith0112@users.noreply.github.com>
2024-04-27 05:49:22 +0530

    Add files via upload

---
+++ myapp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('findbus', views.findbus, name="findbus"),
+    path('findbus1', views.findbus1, name="findbus1"),
+    path('bookings', views.bookings, name="bookings"),
+    path('cancellings', views.cancellings, name="cancellings"),
+    path('seebookings', views.seebookings, name="seebookings"),
+    path('signup', views.signup, name="signup"),
+    path('signin', views.signin, name="signin"),
+    path('success', views.success, name="success"),
+    path('signout', views.signout, name="signout"),
+    path('list2', views.list2,name="list2"),
+    path('feedback', views.feedback, name="feedback"),
+    path('thank2', views.thank2, name="thank2"),
+]
